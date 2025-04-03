a5c9875e79ac6cedb240ef0dd8f3223e9ff10d39
arafatmansuri <mansuriarafat302@gmail.com>
2025-04-03 09:32:34 +0530

    Add Login controller and router

--- BE/src/controllers/user.contoller.js
+++ BE/src/controllers/user.contoller.js
@@ -1,10 +1,20 @@
 const { readFile, writeFile } = require("../db/fileHandler.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 async function encryptPassword(password) {
   return await bcrypt.hash(password, 10);
 }
-async function comparePassword(password, enPassword) {
-  return await bcrypt.compare(password, enPassword);
+function comparePassword(password, enPassword) {
+  return bcrypt.compareSync(password, enPassword);
+}
+function generateAccess_RereshToken(username) {
+  const accessToken = jwt.sign({ username }, process.env.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ username }, process.env.JWT_REFSECRET, {
+    expiresIn: "1d",
+  });
+  return { accessToken, refreshToken };
 }
 async function register(req, res) {
   try {
@@ -35,4 +45,35 @@ async function register(req, res) {
       .json({ message: "Something went wrong from our side" });
   }
 }
-module.exports = { register };
+
+async function login(req, res) {
+  const { username, password } = req.body;
+  if ([username, password].some((field) => field === "")) {
+    return res.status(301).json({ message: "Username/password compulsory" });
+  }
+  const users = await readFile();
+  if (!users.find((user) => user.username === username)) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  const userIndex = users.findIndex((user) => user.username === username);
+  if (!comparePassword(password, users[userIndex].password)) {
+    return res.status(404).json({ message: "Invalid password" });
+  }
+  const { accessToken, refreshToken } = generateAccess_RereshToken(username);
+  users[userIndex].refreshToken = refreshToken;
+  await writeFile(users);
+  const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    path: "/",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  };
+  return res
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .status(200)
+    .json({ message: "User logged in successfully" });
+}
+
+module.exports = { register, login };
--- BE/src/routes/user.route.js
+++ BE/src/routes/user.route.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { register } = require("../controllers/user.contoller.js");
+const { register, login } = require("../controllers/user.contoller.js");
 
 const userRouter = Router();
 
 userRouter.route("/register").post(register);
+userRouter.route("/login").post(login);
 
 module.exports = userRouter;
