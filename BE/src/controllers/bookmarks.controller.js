544c81f319a658b64cac31cbe75ff19bd3d9fa13
arafatmansuri <mansuriarafat302@gmail.com>
2025-04-04 18:50:05 +0530

    Add DeleteBookmark controller and route

--- BE/src/controllers/bookmarks.controller.js
+++ BE/src/controllers/bookmarks.controller.js
@@ -25,11 +25,49 @@ async function addBookmark(req, res) {
       .status(200)
       .json({ message: "Bookmark created successfully", newBookmark });
   } catch (err) {
-    return res
-      .status(500)
-      .json({
-        message: err.message || "Something went wrong while creating bookmark",
-      });
+    return res.status(500).json({
+      message: err.message || "Something went wrong while creating bookmark",
+    });
   }
 }
-module.exports = { addBookmark };
+async function displayAllBookmarks(req, res) {
+  const user = req.user;
+  const bookmarks = user.bookmarks;
+  return res.status(200).json({
+    message: "Bookmarks fetched successfully",
+    user: user.username,
+    bookmarks,
+  });
+}
+async function deleteBookmark(req, res) {
+  try {
+    const bookmarkId = Number(req.params.id);
+    if ([bookmarkId].some((feild) => feild === "")) {
+      return res
+        .status(304)
+        .json({ message: "Bookmark url must not be empty" });
+    }
+    const users = req.users;
+    const userIndex = req.userIndex;
+    const bookmarkIndex = users[userIndex].bookmarks.findIndex(
+      (bm) => bm.id === bookmarkId
+    );
+    if (bookmarkIndex == -1) {
+      return res.status(500).json({
+        message:
+          "Something went wrong from our side while deleting category,Id not found",
+      });
+    }
+    const deletedBookmark = users[userIndex].bookmarks[bookmarkIndex];
+    users[userIndex].bookmarks.splice(bookmarkIndex, 1);
+    await writeFile(users);
+    return res
+      .status(200)
+      .json({ message: "Bookmark deleted successfully", deletedBookmark });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message || "Something went wrong while creating bookmark",
+    });
+  }
+}
+module.exports = { addBookmark, displayAllBookmarks, deleteBookmark };
--- BE/src/routes/bookmarks.route.js
+++ BE/src/routes/bookmarks.route.js
@@ -1,8 +1,14 @@
 const { Router } = require("express");
-const { addBookmark } = require("../controllers/bookmarks.controller.js");
+const {
+  addBookmark,
+  displayAllBookmarks,
+  deleteBookmark,
+} = require("../controllers/bookmarks.controller.js");
 
 bookmarkRouter = Router();
 
 bookmarkRouter.route("/add").post(addBookmark);
+bookmarkRouter.route("/display").get(displayAllBookmarks);
+bookmarkRouter.route("/delete/:id").delete(deleteBookmark);
 
 module.exports = bookmarkRouter;
