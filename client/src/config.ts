83c47d638c49fc353448321a16efb70e975b074b
arafatmansuri <mansuriarafat302@gmail.com>
2025-10-10 16:59:44 +0530

    change backend url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000/api/v1";
+export const BACKEND_URL = "https://bookmark-manager-ox3s.vercel.app/api/v1";
+// export const BACKEND_URL = "http://localhost:3000/api/v1";
