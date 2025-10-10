05a91f43e7013cb39469ecfa6fc665f555107a66
arafatmansuri <mansuriarafat302@gmail.com>
2025-10-10 16:55:27 +0530

    Fix the index

--- BE/api/index.ts
+++ BE/api/index.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../dist/app"; // path to compiled JS
+import app from "../src/app"; // path to compiled JS
 
 export default (req: VercelRequest, res: VercelResponse) => {
   return app(req, res); // forward request to Express
